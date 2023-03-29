1732abbb1386903a4db8d0c62b257b59b81148a2
chinmay021 <chinmaykumar021@gmail.com>
2023-03-29 13:14:35 +0530

    fixed shimmer

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -2,17 +2,16 @@ const Shimmer = () => {
   return (
     <>
       <div className='flex flex-wrap' data-testid='shimmer'>
-        {Array(10)
+        {Array(12)
           .fill([])
           .map((e, index) => (
-            <div key={index} className=' m-3 p-3 w-72 h-72'>
-              <div className='w-full h-1/2 border rounded-sm bg-64 custom-linear-gradient animate-pulse'></div>
+            <div key={index} className=' m-2 p-3 w-80 h-80'>
+              <div className='w-full h-1/2 border rounded-xl bg-64 custom-linear-gradient animate-pulse'></div>
               <div
-                className='w-[90%] h-[10%] mt-3 border rounded-sm
+                className='w-[90%] h-[10%] mt-3 border rounded-full
                bg-64 custom-linear-gradient animate-pulse '
               ></div>
-              <div className='w-[70%] h-[7%]  mt-3 border rounded-sm bg-64 custom-linear-gradient animate-pulse'></div>
-              <div className='w-[70%] h-[7%]  mt-3 border rounded-sm bg-64 custom-linear-gradient animate-pulse'></div>
+              <div className='w-[70%] h-[7%]  mt-3 border rounded-full bg-64 custom-linear-gradient animate-pulse'></div>
             </div>
           ))}
       </div>
