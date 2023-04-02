34284388c5afc8dd6a45311e62ad5484a6de627e
chinmay021 <chinmaykumar021@gmail.com>
2023-04-02 21:10:34 +0530

    fixed active tag background 👍

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -1,15 +1,13 @@
-import { tags } from '../utils/constants';
+import { tags } from "../utils/constants";
 // import { BiChevronLeft, BiChevronRight } from 'react-icons/bi';
-import { useEffect, useState } from 'react';
-import { handleScroll } from '../utils/helper';
-import { useSelector, useDispatch } from 'react-redux';
-import {
-  changeCategory,
-} from '../utils/categorySlice';
+import { useEffect, useState } from "react";
+import { handleScroll } from "../utils/helper";
+import { useSelector, useDispatch } from "react-redux";
+import { changeCategory } from "../utils/categorySlice";
 
 const Tags = () => {
   const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
-  const [active, setActive] = useState('All');
+  const [active, setActive] = useState("All");
 
   const dispatch = useDispatch();
 
@@ -21,34 +19,34 @@ const Tags = () => {
   };
 
   useEffect(() => {
-    handleScroll('tags-wrapper');
+    handleScroll("tags-wrapper");
   }, []);
   return (
     <div
       className={`tags mx-4 flex text-sm items-center ${
         isMenuOpen
-          ? 'lg:w-[calc(100vw-19rem)] w-[calc(100vw-8rem)]'
-          : 'lg:w-[calc(100vw-8rem)] w-[calc(100vw-3rem)]'
+          ? "lg:w-[calc(100vw-19rem)] w-[calc(100vw-8rem)]"
+          : "lg:w-[calc(100vw-8rem)] w-[calc(100vw-3rem)]"
       } min-w-[250px]
       pt-2`}
     >
-
-      <div className='tags-wrapper flex w-full overflow-x-hidden overflow-y-hidden '>
+      <div className="tags-wrapper flex w-full overflow-x-hidden overflow-y-hidden ">
         {tags.map((tag, index) => {
           return (
             <button
               className={`tag  px-3 w-fit py-2 mx-2 cursor-pointer rounded-lg bg-gray-100  dark:text-white dark:bg-zinc-800 ${
-                active === tag ? 'bg-slate-900 text-white dark:bg-white dark:text-zinc-900' : ''
+                active === tag
+                  ? "bg-slate-900 text-white dark:bg-slate-50 dark:text-zinc-900"
+                  : ""
               }`}
               key={index}
               onClick={() => handleSetHomeVideoByKeyword(tag)}
             >
-              <span className='whitespace-nowrap'>{tag}</span>
+              <span className="whitespace-nowrap">{tag}</span>
             </button>
           );
         })}
       </div>
-
     </div>
   );
 };
